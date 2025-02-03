21944c9405ca3625278bcc579606769e22d60fa1
Juan José Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2025-02-03 17:04:26 -0300

    Move src to lib

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./lib/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
